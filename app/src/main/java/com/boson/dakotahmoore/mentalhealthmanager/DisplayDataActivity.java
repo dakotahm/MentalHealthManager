80ad3c37684116c6f54626726d3e8577c27e4a89
dakotahm <dakotah.moore523@gmail.com>
2017-03-04 21:49:12 -0500

    Added more Layouts and some custom drawables

--- app/src/main/java/com/boson/dakotahmoore/mentalhealthmanager/DisplayDataActivity.java
+++ app/src/main/java/com/boson/dakotahmoore/mentalhealthmanager/DisplayDataActivity.java
@@ -6,15 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class DataCollectActivity extends AppCompatActivity {
+public class DisplayDataActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_collect);
+        setContentView(R.layout.activity_display_data);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,27 +42,5 @@ public class DataCollectActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_data_collect, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
